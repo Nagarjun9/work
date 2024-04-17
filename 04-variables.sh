42a97fc16ec8788f7f757d6a8ec91512bc53daef
Nagarjun9 <nagunaggu@gmail.com>
2024-04-17 17:08:33 +0530

    this is 4th variable

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#decleare of varible
+PERSONE1=$1
+PERSONE2=$2
+
+#referning variable
+echo "PERSONE1:: Hi PERSONE2, how are you"
+echo "PERSONE2:: I am good, what about you"
+echo "PERSONE1:: everything okay, what about your job"
+echo "PERSONE2:: its okay, i want upgrade myskil"
+echo "PERSONE1:: that is good choice"
