d2f53eb11395f07cca2bae6582a6999b275a3404
Nagarjun9 <nagunaggu@gmail.com>
2024-04-17 17:29:41 +0530

    05 variable

---
+++ 05-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Please enter Username"
+
+read -s Username #username is variable 
+
+echo "Please enter Password"
+
+read -s Passwrd 
+
+echo "Username is: $Username, Password is: $Password"
