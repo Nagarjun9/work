9edcfc757746138ee63623df316d9fa71d1a92e6
Nagarjun9 <nagunaggu@gmail.com>
2024-04-17 19:14:52 +0530

    08 01

---
+++ 08-special-var.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "All variable: $@"
+echo "Number of variable passed: $#"
+echo "Script name: $0"
+echo "Current working directoy: $PWD"
+echo "current Home: $HOME"
+echo "Which user running the script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "Process id of current shell script: $$"
+echo "process ID of last background commend: $!"
