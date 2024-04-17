66061bc97b12b5d062e7030b1a1a0ed704a93309
Nagarjun9 <nagunaggu@gmail.com>
2024-04-17 17:14:43 +0530

    04 updated changes

--- 04-variables.sh
+++ 04-variables.sh
@@ -5,8 +5,8 @@ PERSONE1=$1
 PERSONE2=$2
 
 #referning variable
-echo "PERSONE1:: Hi PERSONE2, how are you"
-echo "PERSONE2:: I am good, what about you"
-echo "PERSONE1:: everything okay, what about your job"
-echo "PERSONE2:: its okay, i want upgrade myskil"
-echo "PERSONE1:: that is good choice"
+echo "$PERSONE1:: Hi $PERSONE2, how are you"
+echo "$PERSONE2:: I am good, what about you"
+echo "$PERSONE1:: everything okay, what about your job"
+echo "$PERSONE2:: its okay, i want upgrade myskil"
+echo "$PERSONE1:: that is good choice"
