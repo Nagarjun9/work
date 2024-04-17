52dfcec34e6938bffd61e2f4e313a1abd5e427a6
Nagarjun9 <nagunaggu@gmail.com>
2024-04-17 19:32:23 +0530

    09 02

--- 09-condition.sh
+++ 09-condition.sh
@@ -2,9 +2,19 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 10]
+if [ $NUMBER -gt 10 ]
 then 
    echo "Given number $NUMNER is grater then 10"
 else 
-    echo "Given numner $NUMBER is less then 10"
+    echo "Given number $NUMBER is less then 10"
+fi
+
+
+NUMBER=$9
+
+if [ $NUMBER -lt 10]
+then 
+    echo "Given number $NUMBER less then 10"
+else 
+    echo "Given number $NUMBER grater then 10"
 fi
