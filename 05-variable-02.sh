a2387f99c43e953730f81a2d768ab404c6992ecc
Nagarjun9 <nagunaggu@gmail.com>
2024-04-17 17:49:40 +0530

    05 varaiable 02

---
+++ 05-variable-02.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+Username=$1
+Password=$2
+
+echo "Please enter Username"
+
+read -s $Username
+
+echo "Please enter Password"
+
+read -s $Password
+
+print "Username is: $1, Password is: $2"
