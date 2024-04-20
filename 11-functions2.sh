2112d650d9f6f0434ab77b7c3a6206c8e3419918
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 17:34:31 +0530

    10

--- 11-functions2.sh
+++ 11-functions2.sh
@@ -1,4 +1,7 @@
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 VALIDATION(){
     if [ $1 -ne 0 ]
@@ -18,15 +21,15 @@ else
     echo "you are a super user"
 fi
 
-dnf remove mysql -y 
+dnf remove mysql -y &>>LOGFILE
 VALIDATION $? "deinstalation of mysql"
 
-dnf remove git -y 
+dnf remove git -y &>>LOGFILE
 VALIDATION $? "deinstalation of git"
 
-dnf install mysql -y
+dnf install mysql -y &>>LOGFILE
 VALIDATION $? "instalation of mysql"
 
-dnf install git -y 
+dnf install git -y &>>LOGFILE
 VALIDATION $? "instalation of git"
 
