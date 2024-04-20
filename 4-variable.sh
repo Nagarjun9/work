5882fd0af6d27788d25d72c5f7b4d43179563cc0
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 07:37:01 +0530

    4 new file

---
+++ 4-variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#declear the variable outside the script
+PERSON1=$1    #no space = and value 
+PERSON2=$2
+
+#referring the variables
+echo "$PERSON1:: Hi $PERSON1, How are you,"
+echo "$PERSON2:: I am fine, what about you,"
+echo "$PERSON1:: Me too, how is your work,"
+echo "$PERSON2:: Fine man, i want change my present company"
+echo "$PERSON1:: any issue with team or comany"
+echo "$PERSON2:: layoff running, i want learn newthings & change"
+echo "$PERSON1:: then learn Devops and change the orignation"
