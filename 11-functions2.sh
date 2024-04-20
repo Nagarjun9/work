9f4e47c3d84f63426391e69c09486c4140676cb5
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 17:40:42 +0530

    10

--- 11-functions2.sh
+++ 11-functions2.sh
@@ -2,14 +2,18 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 
 VALIDATION(){
     if [ $1 -ne 0 ]
     then 
-        echo "$2..failed"
+        echo "$2..$R failed $N"
         exit 1
     else 
-        echo "$2..success"
+        echo "$2..$G success $N"
     fi
 }
 
