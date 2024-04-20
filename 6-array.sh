8b6596f16736dd864a1aab2e6b994e84d8f9368f
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 10:10:19 +0530

    5 varia

--- 6-array.sh
+++ 6-array.sh
@@ -5,3 +5,11 @@ MOVIES=("RAJA" "BALU" "TILLU")
 echo "First moive is: ${MOVIES[0]}"
 echo "second movie is: ${MOVIES[1]}"
 echo "all movies is: ${MOVIES[@]}"
+
+
+NAMES=("Naga" "Nani" "Naji" "Nari")
+
+echo "first name is: ${NAMES[0]}"
+echo  "second name is: ${NAMES[1]}"
+echo "Third name is: ${NAMES[2]}"
+echo "ALL names is: ${NAMES[@]}"
