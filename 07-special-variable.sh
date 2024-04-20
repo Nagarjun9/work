8329de0c7f7322ab5d4a313fbed17f914e7b1573
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 10:24:08 +0530

    5 varia

---
+++ 07-special-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "all variables: $@"
+echo "Number of variable passed: $#"
+echo "script name: $0"
+echo "current working direcoty: $PWD"
+echo "Home directoy of current user: $HOME"
+echo "which use run the script: $USER"
+echo "Hostname: $HOSTNAME"
+echo "process id of current shell script: $$"
+echo "process id of last backend script: $!"
