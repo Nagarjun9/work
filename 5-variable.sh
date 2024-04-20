7e4d956028f4c5c278bfdc6ebed61528400b0565
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 08:24:05 +0530

    5 3rd

--- 5-variable.sh
+++ 5-variable.sh
@@ -9,3 +9,16 @@ echo "Please enter the Password::"
 read -s  $PASSWORD
 
 echo "Username is: $USERNAME, Password is: $PASSWORD"
+
+
+#!/bin/bash
+
+echo "Please enter username::"
+
+read -s USERNAME #here USERNAME is variable
+
+echo "Please enter password::"
+
+read -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
