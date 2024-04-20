661da03e35d9165794dcb755b2c43bec43bf3755
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 15:40:56 +0530

    10

--- 10-deinstall.sh
+++ 10-deinstall.sh
@@ -2,18 +2,18 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0]
+if [ $USERID -ne 0 ]
 then 
-    echo " you are not super user please use root crediantional"
+    echo "you are not super user please use root crediantional"
     exit 1
 else
     echo "you are super user"
 
 dnf remove mysql -y 
 
-if [ $? -ne 0]
+if [ $? -ne 0 ]
 then 
-    echo " deinstalation of your mysql..failed"
+    echo "deinstalation of your mysql..failed"
     exit 1
 else  
     echo " deinstalation of your mysql...success"
