d4970e56a1e1d388b467b9f82d46b5c2d7cea895
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 15:52:29 +0530

    10

--- 10-deinstall.sh
+++ 10-deinstall.sh
@@ -19,3 +19,23 @@ then
 else  
     echo " deinstalation of your mysql...success"
 fi 
+
+dnf remove git -y 
+
+if [ $? -ne 0 ]
+then 
+    echo "deinstalation of git...failed"
+    exit 1
+else 
+    echo "deinstalation of git..success"
+fi 
+
+dnf install git -y 
+
+if [ $? -ne 0 ]
+then 
+    echo "instalation of git...failed"
+    exit 1
+else 
+    echo "instalatioon of git...success"
+fi 
