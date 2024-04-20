68614b43889f82f1567b542ceb2cf774ab624957
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 10:55:22 +0530

    5 varia

--- 8-condition.sh
+++ 8-condition.sh
@@ -9,5 +9,11 @@ else
     echo "given number is less then 10"
 fi
 
+if [ NUMBER -lt 10]
+then 
+   echo "given number is less then 10"
+else
+   echo "given number is grater then 10"
+fi
 
 echo " number of variable passed $#"
