7c85f04cd02bea50e3eb7bbeb122935efe644ac5
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 07:15:03 +0530

    2 first

---
+++ 2-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Rman:: How are you Raj"
+echo "Raj:: Fine, what about you, how is your work"
+echo "Rman:: i am fine, work also good" 
+echo "Raj::  Recently i started upgrade my self to learn new thing"
+echo "Rman:: realy what are the things you learned recently"
+echo "Rah:: i am started learning Devops"
