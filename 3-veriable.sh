b88900b9733a06c6fadf3ea9d1bdb419cbbf818e
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 07:25:09 +0530

    3

---
+++ 3-veriable.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#declare the variables
+PERSON1=RAJ    #no space = and value 
+PERSON2=KRISH
+
+echo "$PERSON1:: Hi $PERSONE1, How are you,"
+echo "$PERSON2:: I am fine, what about you,"
+echo "$PERSON1:: Me too, how is your work,"
+echo "$PERSON2:: Fine man, i want change my present company"
+echo "$PERSON2:: very bad, i want learn newthings & change"
+echo "$PERSON1:: then learn Devops and change the orignation"
