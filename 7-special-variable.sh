1872de594fd650926961330f35a2720164d1ac91
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 10:29:40 +0530

    5 varia

--- 7-special-variable.sh
+++ 7-special-variable.sh
@@ -8,4 +8,6 @@ echo "Home directoy of current user: $HOME"
 echo "which use run the script: $USER"
 echo "Hostname: $HOSTNAME"
 echo "process id of current shell script: $$"
+sleep 90 &
 echo "process id of last backend script: $!"
+
