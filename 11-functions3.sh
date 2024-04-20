ce77a6e3001b3873c3292337278d0c26af3bbd45
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 22:27:51 +0530

    11

--- 11-functions3.sh
+++ 11-functions3.sh
@@ -4,9 +4,9 @@ USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
 FILENAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$FILENAME-$TIMESTAMP.log
-R="e\[31m"
-G="e\[32m"
-N="e\[0m"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 
 VALIDATION(){
