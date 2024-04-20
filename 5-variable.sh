aa5a9a4b2d853ba0762aa62c7b32140e642cea94
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 07:56:09 +0530

    5

---
+++ 5-variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+Username=$1
+Password=$2
+
+echo "please enter Username"
+
+read $Username
+
+echo "please enter Password"
+
+read $Password
+
+echo "Username is: $Username, Password is: $Password"
