f88586e7f6e3f42e023f680ce55d545c866b7102
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 16:47:54 +0530

    10

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,33 +1,24 @@
-#!/bin/bash 
+#1/bin/bash
 
 
 VALIDATION(){
-    if [ $1 -ne 0 ]
-    then 
-        echo "$2..Failed"
-    else 
-        echo "$2..success"
-    fi 
+         if [ $1 -ne 0 ]
+         then 
+             echo "$2...failed"
+             exit 1
+         else
+             echo "$2...success"  
+         fi   
 }
 
 USERID=$(id -u)
-
 if [ $USERID -ne 0 ]
-then 
-    echo "you are not superuser please run the script with root user"
+then
+    echo "you are not a super user please use root credeiantila"
     exit 1
 else 
     echo "you are a super user"
-fi
-
-dnf install mysql -y
-VALIDATION $? "instalation of mysql"
-
-
-if [ $? -ne 0 ]
-then 
-    echo "instlation on mysql...failed"
-    exit 1
-else 
-    echo "instlation of mysql...success"
 fi 
+
+dnf install mysql -y 
+VALIDATION $? "instalation of mysql"
