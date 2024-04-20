6a2a247edf790390eed0b4a0a66f2f0649b49718
Nagarjun9 <nagunaggu@gmail.com>
2024-04-20 17:08:39 +0530

    10

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,5 +1,10 @@
 #1/bin/bash
 
+USERID=$(id -u)
+TIME=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIME.log
+
 
 VALIDATION(){
          if [ $1 -ne 0 ]
@@ -11,7 +16,7 @@ VALIDATION(){
          fi   
 }
 
-USERID=$(id -u)
+
 if [ $USERID -ne 0 ]
 then
     echo "you are not a super user please use root credeiantila"
@@ -20,15 +25,15 @@ else
     echo "you are a super user"
 fi 
 
-dnf remove mysql -y 
+dnf remove mysql -y &>>$LOGFILE
 VALIDATION $? "deinstalation of mysql"
 
-dnf remove git -y
+dnf remove git -y &>>$LOGFILE
 VALIDATION $? "deinstalation of git"
 
-dnf install mysql -y 
+dnf install mysql -y &>>$LOGFILE
 VALIDATION $? "instalation of mysql"
 
-dnf install git -you
+dnf install git -y  &>>$LOGFILE
 VALIDATION $? "instalation of git"
 
