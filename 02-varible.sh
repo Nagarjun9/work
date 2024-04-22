73cf803c10e6df06c7a1788022b275fde3cb0efb
Nagarjun9 <nagunaggu@gmail.com>
2024-04-22 22:47:00 +0530

    shell

--- 02-varible.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Ramesh:: Hello Suresh, how are you"
-echo "Suresh:: I am good, what about you"
-echo "Ramesh:: i am fine too, how is your work"
-echo "Suresh:: not bad, i am thing to upgrade to Devops"
--- 06-array-01.sh
+++
@@ -1,5 +0,0 @@
-MOVIES=("nag" "ram" "rman")
-
-echo "first movie is: ${MOVIES[1]}"
-echo "FIrst movie is: ${MOVIES[@]}"
-echo "first movie is: ${MOVIES[0]}"
