5d36525f0302d70671dbcb4b308603dd5730f8c5
Nagarjun9 <nagunaggu@gmail.com>
2024-04-22 20:25:54 +0530

    shell

--- 13-install-pack.sh
+++ 13-install-pack.sh
@@ -1,4 +1,7 @@
 USERID=$(id -u)
+TIMESTAME=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAME.log
 
 if [ $USERID -ne 0 ]
 then 
@@ -8,11 +11,20 @@ else
 fi 
 
 
-# echo "all variables $@" #if i give outside variables its show 
+# echo "all variables $@" # 1st if i give outside variables its show 
 
-# now i need to loop the all vaiable use for loop 
+# now i need to loop the all vaiables/parameters use for loop 
+#for i in $@
+#do 
+ #  echo "package to install: $i"
+#done 
+
+
+#now we need to check package installed or not 
+
+
 for i in $@
 do 
    echo "package to install: $i"
+   dnf list installed $i &>>$LOGFILE
 done 
-
