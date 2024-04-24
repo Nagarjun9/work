0c411e004943de1fc99512b73287fba60c3d8a97
Nagarjun9 <nagunaggu@gmail.com>
2024-04-24 19:45:53 +0530

    new

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -5,6 +5,6 @@ COURSE="Devops for current script"
 echo "before calling other script, courese: $COURSE"
 echo "current script process ID: $$"
 
-source ./15-other-script.sh 
+./15-other-script.sh 
 
 echo "after calling other script, course: $COURSE"
