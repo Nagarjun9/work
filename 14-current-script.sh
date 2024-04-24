6f115ea6cefd4a61830d1204ce9ac800b745bb0f
Nagarjun9 <nagunaggu@gmail.com>
2024-04-24 19:30:07 +0530

    new script

--- 14-current-script.sh
+++ 14-current-script.sh
@@ -5,6 +5,6 @@ COURSE="Devops for current script"
 echo "before calling other script, courese: $COURSE"
 echo "current script process ID: $$"
 
-./14-current-script.sh 
+./15-other-script.sh 
 
 echo "after calling other script, course: $COURSE"
---
+++ 15-other-script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COURSE="Devops from current script"
+
+echo "variable value from other script: $COURSE"
+echo "process id of other script: $$"
