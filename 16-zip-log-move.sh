808ee4d4ea6b792ace25fae86d92e89521d7639a
Nagarjun9 <nagunaggu@gmail.com>
2024-04-25 22:16:43 +0530

    combine

--- 16-zip-log-move.sh
+++ 16-zip-log-move.sh
@@ -1,7 +1,71 @@
 #!/bin/bash 
 
-#SOURCE_DIRECTORY=/tmp/app_dir
-#$TARGET_DIRECTORY=/tmp/app_dir/zip-move
+#direct directory give and gip, move the files 
+SOURCE_DIRECTORY=/tmp/app_dir
+TARGET_DIRECTORY=/tmp/app_dir/zip-move
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+TIMESTAME=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAME.log
+
+
+if [ -d $SOURCE_DIRECTORY ]
+then
+   echo "you source directory exits"
+else 
+   echo " you need to create the directory "
+fi 
+
+FILE=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+while IFS= read -r line 
+do 
+  #echo "Deleting file: $line"
+#  rm -rf $line  # remove the file 
+   echo -e "$G gziping file: $line $N" &>>$LOGFILE
+   gzip $line &>>$LOGFILE #gzip the file 
+   
+done <<<$FILE 
+
+FILE1=$(find $SOURCE_DIRECTORY -name "*.gz*" -mtime +14)
+
+while IFS= read -r line 
+do 
+  echo -e "$Y move the files: $line to $TARGET_DIRECTORY $N" &>>$LOGFILE 
+  mv $line $TARGET_DIRECTORY  &>>$LOGFILE
+done <<<$FILE1 
+
+#LOCATION=$(find / -type d -iname "zip-move")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+==========================================
+
+#location to find & zip, move script 
 
 SOURCE_LOCATION=$(find / -type d -iname "app_dir") 
 FIND_LOCATION=$(find / -type d -iname "zip-move") # find the location of zip-move folder 
@@ -31,7 +95,7 @@ do
 #  rm -rf $line  # remove the file 
    echo -e "$G gziping file: $line $N" &>>$LOGFILE
    gzip $line &>>$LOGFILE #gzip the file 
-   mv *.gz *.gz-$TIMESTAME  &>>$LOGFILE
+   
 done <<<$FILE 
 
 FILE1=$(find $SOURCE_LOCATION -name "*.gz*" -mtime +14)
