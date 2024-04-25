6a567938dd58185edd5c7840305407df3cc35df9
Nagarjun9 <nagunaggu@gmail.com>
2024-04-25 18:25:14 +0530

    work

---
+++ 16-delete-log.sh
@@ -0,0 +1,24 @@
+#!/bin/bash 
+
+SOURCE_DIRECTORY=/tmp/app_dir
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[34m"
+
+
+IF [ -d $SOURCE_DIRECTORY ]
+then
+   echo "you source directory exits"
+else 
+   echo " you need to create the directory "
+fi 
+
+FILE=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
+
+while IFS= read -r line 
+do 
+  echo "Deleting file: $line"
+  rm -rf $line 
+done <<<$FILE 
