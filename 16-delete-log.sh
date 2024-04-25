9a9a7a19be96498e4cc164e858f91961c249a86b
Nagarjun9 <nagunaggu@gmail.com>
2024-04-25 19:04:08 +0530

    combine

--- 16-delete-log.sh
+++ 16-delete-log.sh
@@ -20,5 +20,6 @@ FILE=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 while IFS= read -r line 
 do 
   echo "Deleting file: $line"
-  rm -rf $line 
+#  rm -rf $line  # remove the file 
+gzip $line  #gzip the file 
 done <<<$FILE 
