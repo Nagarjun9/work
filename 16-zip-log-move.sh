cc8cf905fcfdc8460df8b998f540f50b88e6e474
Nagarjun9 <nagunaggu@gmail.com>
2024-04-25 21:16:15 +0530

    combine

--- 16-zip-log-move.sh
+++ 16-zip-log-move.sh
@@ -10,6 +10,10 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[34m"
 
+TIMESTAME=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 |cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAME.log
+
 
 if [ -d $SOURCE_DIRECTORY ]
 then
@@ -24,16 +28,17 @@ while IFS= read -r line
 do 
   #echo "Deleting file: $line"
 #  rm -rf $line  # remove the file 
-   echo "gziping file: $line"
-   gzip $line  #gzip the file 
+   echo -e "$G gziping file: $line $N"
+   gzip $line &>>$LOGFILE #gzip the file 
+
 done <<<$FILE 
 
 FILE1=$(find $SOURCE_DIRECTORY -name "*.gz" -mtime +14)
 
 while IFS= read -r line 
 do 
-  echo "move the files: $line"
-  mv $line $FIND_LOCATION
+  echo -e "$Y move the files: $line &N"
+  mv $line $FIND_LOCATION &>>$LOGFILE
 done <<<$FILE1 
 
 #LOCATION=$(find / -type d -iname "zip-move")
