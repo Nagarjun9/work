4606dfd186842ec3ccaac02fbe24c4263777ffc5
Nagarjun9 <nagunaggu@gmail.com>
2024-04-27 16:55:02 +0530

    work

--- 18-Disk-Usage.sh
+++ 18-Disk-Usage.sh
@@ -23,3 +23,22 @@ echo -e "Massage usage: $MASSAGE"
 echo "$MASSAGE" | mail -s "Diskusage" nagarjun676@gmail.com 
 ###
 
+
+DISKUSGE=$(df -hT |grep xfs)
+THROSHOULD=5
+MASSAGE=""
+
+while IFS= read line 
+do 
+   USAGE=$( echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
+   FOLDER=$( echo $line | awk -F " " '{print $NF}')
+  if [ $DISKUSAGE -ge $USAGE ]
+  then 
+      MASSAGE+=$FOLDER is more then $THROSHOULD, Current usage: $USAGE \n"
+  fi
+done <<<$DISKUSAGE
+
+echo "$MASSAGE" | mail -s "Diskusage1" nagarjun676@gmail.com 
+
+
+
