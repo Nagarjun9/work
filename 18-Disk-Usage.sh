a7b69a02842becd5915ffbaed10311ac9a050a14
Nagarjun9 <nagunaggu@gmail.com>
2024-04-27 16:18:55 +0530

    work

---
+++ 18-Disk-Usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+
+DISKSPACE=$(df -hT | grep xfs)
+DISK_THROSHOLD=6
+MASSAGE=""
+
+while IFS= read -r line 
+do 
+  USAGE=$(echo $line | awk -F " " '{print $6F}')
+  FOLDER=$(echo $line | awk -F " " '{print $NF}')
+  if [ $DISKSPACE -ge $DISK_THROSHOLD ]
+  then 
+      MASSAGE+="$FOLDER is more then $DISK_THROSHOLD, Current usage: $USAGE \n"
+  fi 
+done <<$DISKSPACE
+
+echo -e "Massage usage: $MASSAGE
+
+echo "$MASSAGE" | mail -s "Diskusage" nagarjun67@gmail.com 
