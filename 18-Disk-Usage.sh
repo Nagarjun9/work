87d61c9b47347562ba4256de2ad5dc7d36b0eb4e
Nagarjun9 <nagunaggu@gmail.com>
2024-04-27 16:32:41 +0530

    work

--- 18-Disk-Usage.sh
+++ 18-Disk-Usage.sh
@@ -2,24 +2,6 @@
 
 
 
-DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=6
-MESSAGE=""
-
-while IFS= read -r line
-do
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 )
-    FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]
-    then
-        MESSAGE+="$FOLDER is more than $DISK_THRESHOLD, Current usage: $USAGE \n"
-    fi
-done <<< $DISK_USAGE
-
-echo -e "Message: $MESSAGE"
-
-echo "$MESSAGE" | mail -s "Disk Usage Alert" nagarjun67@gmail.com 
-
 # echo "body" | mail -s "subject" to-address
 
 DISKSPACE=$(df -hT | grep xfs)
@@ -28,16 +10,16 @@ MASSAGE=""
 
 while IFS= read -r line 
 do 
-  USAGE=$(echo $line | awk -F " " '{print $6F}')
-  FOLDER=$(echo $line | awk -F " " '{print $NF}')
-  if [ $USAGE -ge $DISK_THROSHOLD ]
-  then 
+   USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1 ) 
+   FOLDER=$(echo $line | awk -F " " '{print $NF}')
+   if [ $USAGE -ge $DISK_THROSHOLD ]
+   then 
       MASSAGE+="$FOLDER is more then $DISK_THROSHOLD, Current usage: $USAGE \n"
   fi 
-done <<$DISKSPACE
+done <<<$DISKSPACE
 
-echo -e "Massage usage: $MASSAGE
+echo -e "Massage usage: $MASSAGE"
 
-echo "$MASSAGE" | mail -s "Diskusage" nagarjun67@gmail.com 
+echo "$MASSAGE" | mail -s "Diskusage" nagarjun676@gmail.com 
 ###
 
